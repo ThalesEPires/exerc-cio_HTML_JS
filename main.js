c7dcea96dfb48efabc9c0f08b169b43966a94446
Thales Pires <thaedupi@gmail.com>
2023-05-02 18:42:03 -0300

    concluindo o JS

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const form = document.getElementById("formulario");
 
-function numValido(numeroMaior) {
-    const numeroMaior = campo2 > campo1;
+function numValido(campo1, campo2) {
+    return campo2 > campo1;
 }
 
 form.addEventListener('submit', function(e) {
     let formValido = false;
     e.preventDefault();
 
-    const campo1 = document.getElementById("campo1").value;
-    const campo2 = document.getElementById("campo2").value;
-    const mensagemSucesso = document.getElementsByClassName("success-message");
-    const mensagemErro = document.getElementsByClassName("error-message");
+    const campo1 = Number(document.getElementById("campo1").value);
+    const campo2 = Number(document.getElementById("campo2").value);
+    const mensagemSucesso = 'O envio foi um sucesso';
+    const mensagemErro = 'Os valores que você digitou são inválidos';
 
-    formValido = numValido(numeroMaior.value)
-    if (campo2 > campo1) {
+    formValido = numValido(campo1, campo2)
+    if (formValido) {
         alert(mensagemSucesso);
 
     } else {
